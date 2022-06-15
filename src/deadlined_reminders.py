5bcffc23b18fe5806269088afe11cad68558d10c
jvrmlndz <melendezdroz@gmail.com>
2022-06-15 15:28:40 -0400

    .

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractmethod, ABC
 from collections.abc import Iterable
+from dateutil.parser import parse
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
 
@@ -12,3 +13,9 @@ class DeadlinedReminder(ABC, Iterable):
     @abstractmethod
     def is_due(self):
         pass
+
+class DateReminder(DeadlinedReminder):
+    
+    def __init__(self, text, date):
+        self.date = parse(date, dayfirst=True)
+        
