349b55b9e016bab5effd205e5a519a31415fa740
jvrmlndz <melendezdroz@gmail.com>
2022-06-15 15:21:23 -0400

    .

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -2,7 +2,7 @@ from abc import ABCMeta, abstractmethod
 from collections.abc import Iterable
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
-    
+
     @abstractmethod
-    def is_due():
+    def is_due(self):
         pass
