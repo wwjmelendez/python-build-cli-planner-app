b57196c2212b5b529f5ce382e782e7f2e13b6525
jvrmlndz <melendezdroz@gmail.com>
2022-06-15 15:43:02 -0400

    .

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 import csv
 from src.reminder import PoliteReminder
+from src.deadlined_reminders import DateReminder
 
 def list_reminders():
     f = open("reminders.csv", "r")
@@ -13,10 +14,12 @@ def list_reminders():
                 print(e.ljust(32), end=' ')
         print()
 
-def add_reminder(text):
+def add_reminder(text, date):
 
-    reminder = PoliteReminder(text)
+    reminder = DateReminder(text, date)
 
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
-        writer.writerow([reminder.text])
+        writer.writerow(reminder)
+
+        
