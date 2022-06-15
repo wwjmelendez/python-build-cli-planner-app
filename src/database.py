ce7a6d024230ff9e1c68baf869b5e000d0d86692
jvrmlndz <melendezdroz@gmail.com>
2022-06-15 15:09:14 -0400

    .

--- src/database.py
+++ src/database.py
@@ -1,4 +1,5 @@
 import csv
+from src.reminder import PoliteReminder
 
 def list_reminders():
     f = open("reminders.csv", "r")
@@ -14,6 +15,8 @@ def list_reminders():
 
 def add_reminder(text):
 
+    reminder = PoliteReminder(text)
+
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
-        writer.writerow([text])
+        writer.writerow([reminder.text])
--- src/reminder.py
+++ src/reminder.py
@@ -5,3 +5,7 @@ class PrefixedReminder:
     def __init__(self, prefix="Hey, don't forget to "):
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
+
+class PoliteReminder(PrefixedReminder):
+    def __init__(text):
+        super().__init__("Please")
