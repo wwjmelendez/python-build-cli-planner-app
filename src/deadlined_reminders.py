dba5702ac32fc324cd7f431d3ba558ccb92e9c4f
jvrmlndz <melendezdroz@gmail.com>
2022-06-15 15:35:02 -0400

    .

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -23,4 +23,9 @@ class DateReminder(DeadlinedReminder):
 
     def is_due(self):
         return self.date <= datetime.now()
+
+    def __iter__(self):
+        return iter([self.date, self.date.isoformat()])
+
+    
             
